265d7dfbbb1c371d2fefea3e1f4c204b450dc623
SteveNelson83 <51002138+SteveNelson83@users.noreply.github.com>
2019-08-28 17:07:15 +0100

    app can get post via api

--- src/components/addProperty.jsx
+++ src/components/addProperty.jsx
@@ -33,37 +33,48 @@ class AddProperty extends React.Component {
     return (
       <div className="AddProperty">
         <h1>Add Property Page</h1>
-        <form onSubmit={this.handleAddProperty}>
-          Title
-          <input name="title" value={this.state.fields.title} onChange={this.handleFieldChange} />
-          Property Type
-          <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
-            <option value="Flat">Flat</option>
-            <option value="Detached">Detached</option>
-            <option value="Semi-Detached">Semi-Detached</option>
-            <option value="Terraced">Terraced</option>
-            <option value="End of Terrace">End of Terrace</option>
-            <option value="Igloo">Igloo</option>
-            <option value="Shack">Shack</option>
-          </select>
-          <br />
-          No. of Bedrooms
-          <input name="bedrooms" value={this.state.fields.bedrooms} onChange={this.handleFieldChange} />
-          No. of Bathrooms
-          <input name="bathrooms" value={this.state.fields.bathrooms} onChange={this.handleFieldChange} />
-          <br />
-          Price(£)
-          <input name="price" value={this.state.fields.price} onChange={this.handleFieldChange} />
-          City
-          <select name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
-            <option value="Manchester">Manchester</option>
-            <option value="Leeds">Leeds</option>
-            <option value="Sheffield">Sheffield</option>
-            <option value="Liverpool">Liverpool</option>
-          </select>
-          <br />
-          Email
-          <input name="email" value={this.state.fields.email} onChange={this.handleFieldChange} />
+        <form id="formbox" onSubmit={this.handleAddProperty}>
+          <span>
+            <div>Title</div>
+            <input name="title" value={this.state.fields.title} onChange={this.handleFieldChange} />
+          </span>
+          <span>
+            <div>Property Type</div>
+            <select name="type" value={this.state.fields.type} onChange={this.handleFieldChange}>
+              <option value="Flat">Flat</option>
+              <option value="Detached">Detached</option>
+              <option value="Semi-Detached">Semi-Detached</option>
+              <option value="Terraced">Terraced</option>
+              <option value="End of Terrace">End of Terrace</option>
+              <option value="Igloo">Igloo</option>
+              <option value="Shack">Shack</option>
+            </select>
+          </span>
+          <span>
+            <div>No. of Bedrooms</div>
+            <input name="bedrooms" value={this.state.fields.bedrooms} onChange={this.handleFieldChange} />
+          </span>
+          <span>
+            <div>No. of Bathrooms</div>
+            <input name="bathrooms" value={this.state.fields.bathrooms} onChange={this.handleFieldChange} />
+          </span>
+          <span>
+            <div>Price</div>
+            £<input name="price" value={this.state.fields.price} onChange={this.handleFieldChange} />
+          </span>
+          <span>
+            <div>City</div>
+            <select name="city" value={this.state.fields.city} onChange={this.handleFieldChange}>
+              <option value="Manchester">Manchester</option>
+              <option value="Leeds">Leeds</option>
+              <option value="Sheffield">Sheffield</option>
+              <option value="Liverpool">Liverpool</option>
+            </select>
+          </span>
+          <span>
+            <div>Email Address</div>
+            <input name="email" value={this.state.fields.email} onChange={this.handleFieldChange} />
+          </span>
           <button type="submit">Add</button>
         </form>
       </div>
--- src/components/properties.jsx
+++ src/components/properties.jsx
@@ -1,9 +1,29 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import axios from 'axios';
 
-const Properties = () => (
-  <Fragment>
-    Properties Page
-  </Fragment>
-);
+class Properties extends React.Component {
+  state = {
+    properties: [],
+  };
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/v1/PropertyListing')
+      .then(res => {
+        const properties = res.data;
+        this.setState({ properties });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Property List</h1>
+        <ul>
+          {this.state.properties.map(property => <l1>{property.title}</l1>)}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Properties;
